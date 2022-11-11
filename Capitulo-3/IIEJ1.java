6da6d8280d07bf9554b2ac3b75e5f86e30ebac24
Victor01val <vvalolm1305@g.educaand.es>
2022-11-11 19:39:02 +0100

    Nuevo

--- Capitulo-3/IIEJ1.java
+++ Capitulo-3/IIEJ1.java
@@ -6,22 +6,21 @@ import java.util.Scanner;
 public class IIEJ1{
 	public static void main (String args[]) {
     
-    Scanner s = new Scanner(System.in);
-    
-    
-    System.out.println("Escribe el primer número");
-    int num_uno = s.nextInt();
-    
-    System.out.println("Escribe el segundo número");
-    int num_dos = s.nextInt();
-    
-    
-    int resultado_multiplicacion = 0;
-    resultado_multiplicacion = num_uno * num_dos;
-    
-    
-    System.out.println("");
-    System.out.println("El resultado de la suma es " + resultado_multiplicacion);
+    try (Scanner s = new Scanner(System.in)) {
+        System.out.println("Escribe el primer número");
+        int num_uno = s.nextInt();
+        
+        System.out.println("Escribe el segundo número");
+        int num_dos = s.nextInt();
+        
+        
+        int resultado_multiplicacion = 0;
+        resultado_multiplicacion = num_uno * num_dos;
+        
+        
+        System.out.println("");
+        System.out.println("El resultado de la suma es " + resultado_multiplicacion);
+    }
     
 	}
 }
